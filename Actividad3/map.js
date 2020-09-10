f9f8366a53f07f9e4d27f6cf4e5be4bca3d1f537
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-09-10 18:02:52 -0500

    cambios

--- Actividad3/map.js
+++ Actividad3/map.js
@@ -1,39 +1,34 @@
     
-    var tilesProvider='	https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var mymap = L.map('mapid').setView([21.152334, -101.7113132], 14);
-
-	L.tileLayer(tilesProvider, {
-		maxZoom: 17,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(mymap);
-
-	var marker=L.marker([21.152334, -101.7113132]).addTo(mymap)
-        .bindPopup("<b>Universidad de la salle bajio!</b><br />").openPopup();
-        
-
-
-    var newicon=L.icon({
-        iconUrl:'ubicacion.png',
-        iconSize:[60,60],
-        iconAnchor:[30,30],
-        popupAnchor:[-3,-76]
-    });
-
-    L.marker([21.152334, -101.7113132],{icon:newicon}).addTo(mymap)
-		.bindPopup("<b>Marcador perzonalizado!</b><br />").openPopup();
-   
-    mymap.doubleClickZoom.disable();
-
-    function onMapClick(e) {
-            popup
-                .setLatLng(e.latlng)
-                .setContent("You clicked the map at " + e.latlng.toString())
-                .openOn(mymap);
-    }
+    const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    let Mapview = L.map('Mapview').setView([21.152334, -101.713132],13)
+    L.tileLayer(tilesProvider,{    maxZoom:17,}).addTo(Mapview)
+    let marker = L.marker([21.152334, -101.713132]).addTo(Mapview)
     
-    mymap.on('click', onMapClick);
+    var newIcon = L.icon({    
+        iconUrl:'ubicacion.png',    
+        iconSize:[60,60],    
+        iconAnchor:[30,60],    
+        popupAnchor:[-3,-76]}
+    )
+    
+    L.marker([21.16, -101.8], {icon: newIcon}).addTo(Mapview);
+    
+    Mapview.doubleClickZoom.disable()
+    var tam=0;
+
+    var posiciones=[]; 
+    Mapview.on('dblclick', function(e){    
+        tam=document.getElementById('tam').value ; 
+        let latLng = Mapview.mouseEventToLatLng(e.originalEvent)    
+        console.log(latLng); 
+        console.log(tam);   
+        L.marker([latLng.lat,latLng.lng],{icon: newIcon}).addTo(Mapview);
+        posiciones.push(latLng);
+        if(posiciones.length>=tam  ){
+            var poligono=null; 
+            poligono=L.polygon([
+                posiciones
+            ]).addTo(Mapview);
+        }
+        
+    });
