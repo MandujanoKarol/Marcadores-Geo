9cefcfdd73ae89df4d99fdbaeef0a0221b40da9a
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-09-11 18:49:47 -0500

    actividad 3 markadores pares o impares

--- Actividad5/map.js
+++ Actividad5/map.js
@@ -1,10 +1,6 @@
-    
     const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     let Mapview = L.map('Mapview').setView([21.152334, -101.713132],13)
     L.tileLayer(tilesProvider,{    maxZoom:17,}).addTo(Mapview) 
-
-
-
     var newIconpar = L.icon({    
         iconUrl:'ubicacionpar.png',    
         iconSize:[60,60],    
@@ -17,13 +13,9 @@
         iconAnchor:[30,60],    
         popupAnchor:[-3,-76]
     });
-     
-    
     Mapview.doubleClickZoom.disable();
-    
     var tam=0; 
     var markers=[];  
-
     Mapview.on('dblclick', function(e){    
         tam++; 
         let latLng = Mapview.mouseEventToLatLng(e.originalEvent)  
@@ -31,6 +23,5 @@
             markers.push(L.marker([latLng.lat,latLng.lng],{icon: newIconpar}).addTo(Mapview)); 
         }else{
             markers.push(L.marker([latLng.lat,latLng.lng],{icon: newIconimpar}).addTo(Mapview)); 
-        }
-        
+        } 
     });  
