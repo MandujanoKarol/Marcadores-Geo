50d0b2afcd733c6991f58f696b6444c86c0f34ba
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-09-14 12:16:55 -0500

    Actividad 3 agregar marcador par o impar doble click

--- Actividad5/map.js
+++ Actividad5/map.js
@@ -1,27 +1,44 @@
-    const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    let Mapview = L.map('Mapview').setView([21.152334, -101.713132],13)
-    L.tileLayer(tilesProvider,{    maxZoom:17,}).addTo(Mapview) 
+    ///Mapa estilos
+    const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    ///centrar mapa en cierta coordenada
+    let Mapview = L.map('Mapview').setView([21.152334, -101.713132],13);
+    ///Agregar a  mapa propiedades
+    L.tileLayer(tilesProvider,{maxZoom:17,}).addTo(Mapview); 
+    ///Iconos de Marcadores
+    ///Icono-Par
     var newIconpar = L.icon({    
         iconUrl:'ubicacionpar.png',    
         iconSize:[60,60],    
         iconAnchor:[30,60],    
         popupAnchor:[-3,-76]
     });
+    ///Icono-Impar
     var newIconimpar = L.icon({    
         iconUrl:'ubicacionimpar.png',    
         iconSize:[60,60],    
         iconAnchor:[30,60],    
         popupAnchor:[-3,-76]
     });
+
+    ///Desactivar docle click zoom
     Mapview.doubleClickZoom.disable();
+
+    ///Contenedor de clicks 
     var tam=0; 
+    ///Arreglo de marcadores
     var markers=[];  
-    Mapview.on('dblclick', function(e){    
+    ///Evento docle click en mapa
+    Mapview.on('dblclick', function(e){  
+        ///Aumentar clicks  
         tam++; 
+        ///obtener las coordenadas de el evento
         let latLng = Mapview.mouseEventToLatLng(e.originalEvent)  
+        ///Verificar si es par o impar
         if(tam%2==0){
+            ///Agregar marcador a mapa icon par
             markers.push(L.marker([latLng.lat,latLng.lng],{icon: newIconpar}).addTo(Mapview)); 
         }else{
+            ///Agregar marcador a mapa icon impar
             markers.push(L.marker([latLng.lat,latLng.lng],{icon: newIconimpar}).addTo(Mapview)); 
         } 
     });  
